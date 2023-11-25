411f5ff99d18a8a511692e5cced69b4cdb076caf
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-25 15:21:39 +0530

    classes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-// custom type creation
-var student1 = { id: 1, name: 'Kavindu', age: 28, faculty: 'CSE' };
-var student2 = { id: 2, name: 'Janith', age: 25, faculty: 'TONIC' };
-var student3 = { id: 3, name: 'Kalum', age: 20, faculty: 'TONIC', uni: 'UoC' };
-var students = [];
-var teachers = [];
-students.push(student1);
-students.push(student2);
-students.push(student3);
+// class
+var Person = /** @class */ (function () {
+    function Person(name, age, nic) {
+        this.name = name;
+        this.age = age;
+        this.nic = nic;
+    }
+    return Person;
+}());
+var Person1 = new Person('kavindu', 28, '200167202516');
+console.log(Person);
---
+++ app.ts
@@ -0,0 +1,16 @@
+// class
+
+class Person{
+    name: string;
+    age: number;
+    nic: string;
+
+    constructor(name: string, age: number, nic: string){
+        this.name=name;
+        this.age=age;
+        this.nic=nic;
+    }
+}
+
+let Person1 = new Person('kavindu',28,'200167202516');
+console.log(Person);
