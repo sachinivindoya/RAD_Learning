fc18ff2229eda719bed7e57e7e2dbe933a9f5076
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-25 12:31:43 +0530

    functions3

--- 4_functions.ts
+++ 4_functions.ts
@@ -13,7 +13,35 @@ function findSumInString(num1: number,num2: number ):string{
  let result1 = findSum(10 ,20);
  console.log(result1);
 
+ //-----------------------------------------------------------------------------
  
  let result2 = findSumInString(10 ,20);
  console.log(result2);
 
+ function findTheS(num1: number | string, num2: number | string): number{
+    if(typeof num1=== 'string' || typeof num2==='string'){ // type guard
+        return +num1 + +num2;
+    }else{
+        return num1+num2;
+    }
+    
+    }
+    
+    let result4 = findTheS(10,30);
+    console.log("result4 :",result4);
+//------------------------------------------------------------------------------
+
+
+    function findTheSum(num1: number | string, num2: number | string): number | string{
+        if(typeof num1=== 'string' || typeof num2==='string'){ 
+            return +num1 + +num2;
+        }else{
+            return num1+num2;
+        }
+        
+        }
+        
+        let result5 = findTheSum(10,50);
+        console.log("result5 :",result5);
+
+        //------------------------------------------------------------
