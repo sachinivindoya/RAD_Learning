42b082568a67157bf1d2fbc4fe33598875dce3ca
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-25 12:37:28 +0530

    objects

---
+++ 5_objects.ts
@@ -0,0 +1,9 @@
+//objects
+
+let student:{
+    id: number;
+    name: string;
+    age: number;
+    collage: string;
+}
+student = {id: 1, name: 'kavidu', age: 28, collage: 'uobolten'};
---
+++ app.js
@@ -0,0 +1,3 @@
+//objects
+var student;
+student = { id: 1, name: 'kavidu', age: 28, collage: 'uobolten' };
