6fdf3e828583ca76d250e8c81cadd14a5e52bf72
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-11-25 15:16:01 +0530

    types

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// arrays
-var num_array = [1, 2, 3, 4];
-var str_array = ['ranil', 'mahinda', 'gota'];
-var str_num_array = [1, 2, 'ranil', 3, 'mahinda', 'gota'];
-var student1 = { id: 1, name: 'kavindu', age: 28, faculty: 'CSE' };
-var student2 = { id: 2, name: 'sachini', age: 22, faculty: 'TONIC' };
+// custom type creation
+var student1 = { id: 1, name: 'Kavindu', age: 28, faculty: 'CSE' };
+var student2 = { id: 2, name: 'Janith', age: 25, faculty: 'TONIC' };
+var student3 = { id: 3, name: 'Kalum', age: 20, faculty: 'TONIC', uni: 'UoC' };
 var students = [];
+var teachers = [];
 students.push(student1);
 students.push(student2);
+students.push(student3);
